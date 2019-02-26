1900b40f340ea8e7be4c89ce12087b77ba3d8991
KeenanTrey <46493050+KeenanTrey@users.noreply.github.com>
2019-02-26 09:46:55 -0700

    1

---
+++ SandboxDemo/WebApp/About.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="WebApp.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h2><%: Title %>.</h2> 
+    <h3>Your application description page.</h3>
+    <p>Use this area to provide additional information. Enter your name and click the button</p>
+    <asp:Label ID="MessageLabel" runat="server" />
+    <%--- Tips:
+        [ctrl + k, ctrl + c will comment out text] 
+        [crtl k ctrl u will uncomment--%>]
+    <%--[ctrl k ctrl d will format--%>
+
+    <%--comment label for general messages--%>
+
+    <%--ID attribute will genrate a field name in the code behind class that allows programming interaction with asp.net server controls--%>
+    <asp:TextBox ID="FullName" runat="server" />
+    <%--A link will look like a link (render as an <a></a>) but work like a button--%>
+    <asp:LinkButton ID="GetInitials" runat="server" CssClass="btn btn-primary" Text="Get Your initals" OnClick="GetInitials_Click" />
+    <%--Onclick specified associates the click event of the button with a method hanfler in the code behind of the page--%>
+    <asp:Label ID="Initials" runat="server" />
+    <asp:LinkButton ID="ReverseName" runat="server" CssClass="btn btn-default" Text="Spell it Backwards" Visible="false" OnClick="ReverseName_Click" />
+</asp:Content>
---
+++ SandboxDemo/WebApp/About.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Is a stage of processing the page where you ge tto set up aspects of the page's content before any of the events of the page's control are processed
+            if (!IsPostBack) // !ISPost Back means a get request
+            {
+                MessageLabel.Text = "Tell us about yourself!";
+            }   //Label     .string
+            else
+            {
+                MessageLabel.Text = string.Empty;
+            }
+        }
+
+        protected void GetInitials_Click(object sender, EventArgs e)
+        {
+            //This method is what gets called when the <asp:LinkButton ID=GetInitials is click
+            //We can say that this method handles click event to that button/link
+
+            //Take the users input and create an array of strings
+            var nameParts = FullName.Text.Split(' ');
+            //Construct the initials by taking the first character of each word part
+            string result = "";
+            foreach(var part in nameParts)
+            {
+                if (!string.IsNullOrWhiteSpace(part))
+                {
+                    result += $"{part[0]}.";
+                }
+                //Place reslut in Label on form
+                Initials.Text = result;
+
+                if (string.IsNullOrEmpty(result))
+                {
+                    MessageLabel.Text = "Cannot get initals from empty text";
+                    MessageLabel.CssClass = "bg-info";
+                }
+                else
+                {
+                    ReverseName.Visible = true;
+                }
+            }
+        }
+
+        protected void ReverseName_Click(object sender, EventArgs e)
+        {
+            string reversed = string.Join("",FullName.Text.ToArray().Reverse());
+            MessageLabel.Text = $"Your name reversed is <b>{reversed}</b>";
+        }
+    }
+}
